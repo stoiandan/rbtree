48e79ecd7bb9f904dd272981ae1ce0248e7518aa
ZbRoby <robi1796@yahoo.com>
2020-12-20 21:08:16 +0200

    Finish leftRotate test cases

--- models/node_test.go
+++ models/node_test.go
@@ -48,33 +48,92 @@ func TestArrayWithDepthOfNodeAndInsertDataList(t *testing.T) {
 	}
 }
 
-//not yet correct expected values
 func TestLeftRotate(t *testing.T) {
 	errorMessages := ""
 
 	errorMessages += testLeftRotate(
 		5,
-		[]int{6, 7, 8, 9, 10},
-		"[5(1) 6(3) 7(56) 8(28) 9(14) 10(29)]",
+		[]int{4, 3, 2},
+		"[2(8) 3(4) 4(2) 5(1)]",
 	)
 	errorMessages += testLeftRotate(
 		5,
-		[]int{-6, 5, 3, 9, 8, 6, -9, 3, 2, 5, 8},
-		"[-9(4) -6(2) 2(10) 3(5) 3(11) 5(1) 5(3) 5(56) 6(28) 8(14) 8(29) 9(7)]",
+		[]int{6, 7, 8},
+		"[5(2) 6(1) 7(3) 8(7)]",
+	)
+	errorMessages += testLeftRotate(
+		5,
+		[]int{3, 8, -3, 4, 7, 10},
+		"[-3(8) 3(4) 4(9) 5(2) 7(5) 8(1) 10(3)]",
+	)
+	errorMessages += testLeftRotateChild(
+		5,
+		[]int{4, 3, 2},
+		[]bool{false},
+		"[2(8) 3(4) 4(2) 5(1)]",
+	)
+	errorMessages += testLeftRotateChild(
+		5,
+		[]int{6, 7, 8},
+		[]bool{true},
+		"[5(1) 6(6) 7(3) 8(7)]",
+	)
+	errorMessages += testLeftRotateChild(
+		5,
+		[]int{3, 8, -3, 4, 7, 10},
+		[]bool{false},
+		"[-3(8) 3(4) 4(2) 5(1) 7(6) 8(3) 10(7)]",
+	)
+	errorMessages += testLeftRotateChild(
+		5,
+		[]int{3, 8, -3, 4, 7, 10},
+		[]bool{true},
+		"[-3(4) 3(2) 4(5) 5(1) 7(12) 8(6) 10(3)]",
 	)
 
 	if errorMessages != "" {
-		t.Error(errorMessages)
+		t.Error("\n" + errorMessages)
 	}
 }
 
 func testLeftRotate(rootData int, dataList []int, expected string) string {
+	return testRotateChild(false, rootData, dataList, []bool{}, expected)
+}
+
+func testLeftRotateChild(rootData int, dataList []int, childDirections []bool, expected string) string {
+	return testRotateChild(false, rootData, dataList, childDirections, expected)
+}
+
+func testRotateChild(rotateRight bool, rootData int, dataList []int, childDirections []bool, expected string) string {
 	root := NewNode(rootData)
 	root.InsertDataList(dataList)
-	root.leftRotate()
+	before := fmt.Sprintf("%v", root.ArrayWithDepth())
+	temp := root
+	var rotate string
+	for _, childDirection := range childDirections {
+		if childDirection {
+			temp = root.Right
+		} else {
+			temp = root.Left
+		}
+	}
+	if rotateRight {
+		temp.rightRotate()
+		rotate = "Right"
+	} else {
+		temp.leftRotate()
+		rotate = "Left"
+	}
+	if root.Parent != nil {
+		root = root.Parent
+	}
 	got := fmt.Sprintf("%v", root.ArrayWithDepth())
 	if got != expected {
-		return fmt.Sprintf("Left Rotate failed!\nExpected:\t%v\nGot:     \t%v\n\n", expected, got)
+		if len(childDirections) > 0 {
+			return fmt.Sprintf("%v Rotate of child failed!\nChild:   \t%v\nBefore:  \t%v\nExpected:\t%v\nGot:     \t%v\n\n", rotate, temp.Data, before, expected, got)
+		} else {
+			return fmt.Sprintf("%v Rotate failed!\nBefore:  \t%v\nExpected:\t%v\nGot:     \t%v\n\n", rotate, before, expected, got)
+		}
 	}
 	return ""
 }
